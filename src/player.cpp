c75c42ec2b1de6519785d52c1468fdd9de68bd94
Miguel Rodrigues Carrilho Borges <up202004481@up.pt>
2022-11-12 16:28:27 +0000

    early prototype Player

--- src/player.cpp
+++ src/player.cpp
@@ -1,3 +1,17 @@
 #include "player.h"
 
+Player create_player(std::string name, bool is_computer, int depth) {
+  Player player;
+  player.name = name;
+  player.is_computer = is_computer;
+  player.depth = depth;
+  player.last_move[0] = 0; 
+  player.last_move[1] = 0; 
+  return player;
+}
 
+void Player::play(int *last_move) {
+  if (is_computer)
+    return;
+  return;
+}
--- src/player.h
+++ src/player.h
@@ -8,9 +8,12 @@
  * The depth parameter will only be used in case is_computer is true. It will specify the depth of the AI (minimax) algorithm.
  */
 typedef struct Player {
-  std::string player;        /* Player Name */
+  std::string name;          /* Player Name */
   int last_move[2];          /* Last executed move (x, y) */
-  void play(int *last_move); /* Receveis input and updates @last_move value */
+  void play(int *last_move); /* Receveis input and updates @last_move and @Board values */
   bool is_computer;          /* Boolean that represents if the player is an AI */
   int depth;                 /* AI depth */
 } Player;
+
+
+Player create_player(std::string name, bool is_computer = false, int depth = 0);
