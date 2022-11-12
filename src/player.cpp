e1b116f68ce6425b107fde124d107f28a0088dee
Miguel Rodrigues Carrilho Borges <up202004481@up.pt>
2022-11-12 20:59:44 +0000

    player almost done, needs ai + add stuff to board

--- src/player.cpp
+++ src/player.cpp
@@ -1,6 +1,13 @@
+#include <cctype>
+#include <iostream>
+#include <cstdlib>
 #include "player.h"
 
-Player create_player(std::string name, bool is_computer, int depth) {
+#ifdef __APPLE__
+#include <cstdio>
+#endif
+
+Player create_player(std::string name, char symbol, bool is_computer, int depth) {
   Player player;
   player.name = name;
   player.is_computer = is_computer;
@@ -11,7 +18,41 @@ Player create_player(std::string name, bool is_computer, int depth) {
 }
 
 void Player::play(int *last_move) {
+  int input;
+  char exit_confirmation;
   if (is_computer)
     return;
-  return;
+
+ask_input:
+  std::cout << "Where do you want to play?\n> " << std::endl;
+  if (std::cin >> input) {
+    /* Codigo para por a input */
+    return;
+  }
+
+  if (!std::cin.eof()) {
+    std::cout << "Invalid input, must be a integer number" << std::endl;
+    std::cin.clear();
+#ifdef __APPLE__
+    clearerr(stdin);
+#endif
+    goto ask_input;
+  }
+
+  do {
+    std::cout << "Do you really want to quit? (Y/N)" << std::endl;
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+#ifdef __APPLE__
+      clearerr(stdin);
+#endif
+    }
+
+    std::cin >> exit_confirmation;
+    exit_confirmation = toupper(exit_confirmation);
+    if (exit_confirmation == 'N') goto ask_input;
+
+  } while (exit_confirmation != 'Y');
+  std::exit(0);
 }
--- src/player.h
+++ src/player.h
@@ -10,10 +10,19 @@
 typedef struct Player {
   std::string name;          /* Player Name */
   int last_move[2];          /* Last executed move (x, y) */
-  void play(int *last_move); /* Receveis input and updates @last_move and @Board values */
   bool is_computer;          /* Boolean that represents if the player is an AI */
   int depth;                 /* AI depth */
+  char symbol;               /* Player symbol in the board */
+  void play(int *last_move); /* Receveis input and updates @last_move and @Board values */
 } Player;
 
 
-Player create_player(std::string name, bool is_computer = false, int depth = 0);
+/**
+ * Function that returns a player that accepts parameters to feed the initial data. Serves as a constructor.
+ * @param name Player name
+ * @param symbol Symbol that will show up in the board
+ * @param is_computer Boolean that should be true if the player isn't human
+ * @param depth Depth of minimax algorith for the AI (only used if @is_computer is true)
+ * @return Player with the given data.
+ */
+Player create_player(std::string name, char symbol, bool is_computer = false, int depth = 0);
