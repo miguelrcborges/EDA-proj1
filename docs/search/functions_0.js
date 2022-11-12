a0e6cfbe531c4f3398417395cfd260386a082531
Miguel Rodrigues Carrilho Borges <up202004481@up.pt>
2022-11-12 23:51:32 +0000

    docs generated

---
+++ docs/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['check_5fwin_0',['check_win',['../structBoard.html#a3a8580d961ab10c21ad248c61c518fb9',1,'Board']]]
+];
---
+++ docs/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "bcp",
+  1: "bp",
+  2: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions"
+};
+
