4e266e87f86ae1b4d837b85fe2ed4ca4788efc1d
Miguel Rodrigues Carrilho Borges <up202004481@up.pt>
2022-11-12 15:50:55 +0000

    first commit

---
+++ makefile
@@ -0,0 +1,30 @@
+CC = gcc
+CXXC = g++
+OUTPUT = bin/Projeto01
+WARNINGS = -Wall -Wextra -Wwrite-strings
+LINKS = 
+CFLAGS = -march=native -O2 -ftree-vectorize -fno-semantic-interposition -fno-plt -pipe -s -flto $(LINKS) $(WARNINGS)
+DEBUG = -g
+
+CSRC = $(wildcard src/*.c)
+CXXSRC = $(wildcard src/*.cpp)
+OBJ = $(patsubst src/%.c, obj/%.o, $(CSRC))
+OBJ := $(OBJ) $(patsubst src/%.cpp, obj/%.o, $(CXXSRC))
+
+
+$(OUTPUT): $(OBJ)
+	$(CXXC) $^ -o $@ $(CFLAGS)
+
+obj/%.o: src/%.c
+	$(CC) $^ -c -o $@ $(CFLAGS)
+
+obj/%.o: src/%.cpp
+	$(CXXC) $^ -c -o $@ $(CFLAGS)
+
+run: $(OUTPUT)
+	bin/Projeto01
+
+
+debug: bin/debug
+bin/debug: src/*.c
+	$(CC) $^ -o $@ $(DEBUG)
---
+++ src/player.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+/**
+ * Our Player type. This type handles user input or computer move computation. It also stores the player name and the last move done,
+ * in order to save computation when checking if the state of the game.
+ *
+ * The depth parameter will only be used in case is_computer is true. It will specify the depth of the AI (minimax) algorithm.
+ */
+typedef struct Player {
+  std::string player;        /* Player Name */
+  int last_move[2];          /* Last executed move (x, y) */
+  void play(int *last_move); /* Receveis input and updates @last_move value */
+  bool is_computer;          /* Boolean that represents if the player is an AI */
+  int depth;                 /* AI depth */
+} Player;
