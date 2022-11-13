60a731428e0798707e211c4538f4eda0fc395297
tomasccpt <tomascurralo2014@gmail.com>
2022-11-13 18:04:15 +0000

    bapati bupiti

--- src/game.cpp
+++ src/game.cpp
@@ -34,12 +34,17 @@ int Game::log_match()
 	out.open("match logs.txt");
 	if (game.board.check_win)
 	{
-		output_string = times_year << " - " << times_mon << " - " << times_day << " / " << times_hour << ":" << times_min 
-			<< " - " << " 1)" << players[0].name << " vs 2)" << players[1].name << " - vencedor : ";
-		if (players->is_computer)
+		out << times_year << " - " << times_mon << " - " << times_day << " / " << times_hour << ":" << times_min<< " - " << " 1)" << players[0].name;
+		if (players[0].is_computer)
 		{
-			output_string = +"(CPU) ";
+			out << "(CPU) ";
 		}
+		out << " vs 2)" << players[1].name;
+		if (players[1].is_computer)
+		{
+			out << "(CPU) ";
+		}
+		out << " - vencedor : ";
 		if (players[turn % 2] == 0)
 		{
 			out << output_string << "1) " << players[0].name;
@@ -47,6 +52,10 @@ int Game::log_match()
 		else
 		{
 			out << output_string << "2) " << players[1].name;
+		}
+		if (players[turn % 2].is_computer)
+		{
+			out << " (CPU)" << std::endl;
 		}
 	}
 	out.close();
